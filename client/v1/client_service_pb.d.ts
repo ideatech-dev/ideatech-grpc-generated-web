df30882f0a6669cf6aceb881af7325d81bbc8a60
Bayu Aditya <bayu.aditya@sci.ui.ac.id>
2021-11-10 21:17:39 +0700

    delete previous version

--- client/v1/client_service_pb.d.ts
+++
@@ -1,8 +0,0 @@
-// package: client.v1
-// file: client/v1/client_service.proto
-
-/* tslint:disable */
-/* eslint-disable */
-
-import * as jspb from "google-protobuf";
-import * as client_v1_question_answer_pb from "../../client/v1/question_answer_pb";
--- organization/v1/organization_service_pb.d.ts
+++
@@ -1,9 +0,0 @@
-// package: organization.v1
-// file: organization/v1/organization_service.proto
-
-/* tslint:disable */
-/* eslint-disable */
-
-import * as jspb from "google-protobuf";
-import * as organization_v1_organization_pb from "../../organization/v1/organization_pb";
-import * as organization_v1_organization_user_pb from "../../organization/v1/organization_user_pb";
